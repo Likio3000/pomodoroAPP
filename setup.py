e73f2f1da688cd68549cb91c86634eb8f018a2a0
Alex <100605452+Likio3000@users.noreply.github.com>
2025-07-04 17:43:30 +0200

    chore: install config module

--- setup.py
+++ setup.py
@@ -2,7 +2,8 @@ from setuptools import setup, find_packages
 
 setup(
     name='pomodoro_app',
-    version='0.1',
+    version='0.1.1',
     packages=find_packages(),
+    py_modules=['config'],
     license="MIT",
 )
